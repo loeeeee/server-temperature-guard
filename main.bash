81f923473a93e9bbafcf9fe970c1165a3c481bdf
Loe Bi <Loeeeee>
2024-02-29 20:41:59 +0800

    add main file

---
+++ main.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#------------------
+# This script needs grep and sensors-lm to function properly
+# This script is intended for water-cooled server whose components are not trusted. It will poeroff tbe server when water temperature becomes unreasonable.
+#------------------
+
+# Get CPU tempeture
+
+CPU_temps_raw=$(sensors | grep Package)
+
+temps_pattern="\b([0-9]{2,3})\.[0-9]{1,2}°C\b" # Reg pattern for CPU temp
+# temps_pattern="([0-9]{2})" # Bash does not support \d for digits
+
+if [[ $CPU_temps_raw =~ $temps_pattern ]]
+then
+    CPU_temps="${BASH_REMATCH[1]}"
+else
+    echo "Failed to get CPU temps"
+fi
+
+
+
+# Judge Severity of condition
+
+# EMERGENCY
+
+if [[ $CPU_temps -ge 70 ]]
+then
+    echo "SEVER OVERHEATING, $CPU_temps"
+    echo "CPU SEVER OVERHEATING, $CPU_temps" > /dev/kmsg
+    echo "Tempeture checker force poweroff" > /dev/kmsg
+    poweroff
+fi
+
+# Immediate attention
+
+if [[ $CPU_temps -ge 60 ]]
+then
+    echo "Overheating, $CPU_temps"
+    echo "CPU Overheating, $CPU_temps" > /dev/kmsg
+    echo "Tempeture checker force shutdown" > /dev/kmsg
+    shutdown
+fi
+
+# Maintainence required
+
+if [[ $CPU_temps -ge 50 ]]
+then
+    echo "Warm, $CPU_temps"
+    echo "CPU Warm, $CPU_temps" > /dev/kmsg
+fi
+
+# Delightful
+
+if [[ $CPU_temps -lt 50 ]]
+then
+    echo "Delightful"
+fi
+
+echo "Tempeture checking loop complete"
